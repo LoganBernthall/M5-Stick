bfe6a0dc0285ac536d99f89c827a27e2047bd1c6
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2026-02-01 21:33:57 +0000

    PS Script to get required PC data
    This project will:
    
    *GET SYS data with powershell
    *Transfer data over WIFI
    *Display on M5Stick

--- PCStatisticReader/PSGetSysInfo.ps1
+++ PCStatisticReader/PSGetSysInfo.ps1
@@ -1,1 +1,29 @@
-﻿
+﻿<#  
+*Created By - Logan Bernthall - 01/02/2026
+*Part of my M5StckProject, Powershell script used to collect sys metrics
+#> 
+
+function GetCPUParams
+{
+
+#Get CPU Load
+$TotalLoads = (wmic cpu get LoadPercentage)
+
+#Get CPU Temp
+$TotalTemp = (Get-CimInstance -Namespace root/wmi -ClassName MsAcpi_ThermalZoneTemperature -Filter "Active='True' and CurrentTemperature<>2732" -Property InstanceName, CurrentTemperature |
+    Select-Object InstanceName, @{n='CurrentTemperatureC';e={'{0:n0} C' -f (($_.CurrentTemperature - 2732) / 10.0)}})
+
+Write-Output $TotalLoads $TotalTemp
+}
+
+function GetRAMParams
+{
+
+#Get Ram usage
+$TotalUsage = (Get-CimInstance -ClassName CIM_OperatingSystem).FreePhysicalMemory / 1024
+Write-Output "Memory Free (MB)" $TotalUsage
+
+}
+
+#Loop every 5 seconds
+while ($true) {GetCPUParams; GetRamParams; Start-Sleep -Seconds 5}
