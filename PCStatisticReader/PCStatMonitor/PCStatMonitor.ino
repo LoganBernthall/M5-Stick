7b6f6b384a0a1817474ba2d6ae23dcde4b482bd5
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2026-02-11 23:07:35 +0000

    Fix bricking issue
    Would not move to next screen

--- PCStatisticReader/PCStatMonitor/PCStatMonitor.ino
+++ PCStatisticReader/PCStatMonitor/PCStatMonitor.ino
@@ -20,63 +20,40 @@ const char* serverURL = "http://192.168.1.50:5000/stats";
 
 void stats()
 {
-  while (true)
-  {
-    stats();    
-    delay(5000); //Sleep 5 seconds
-  }
+
 }
 
 void setup() {
-    //Start
   Serial.begin(115200);
   M5.begin();
-
-  //Variable declaration
-  char greeting[] = "M5Stick PC Stats";
-  char instrucHome[] = "Press Home To Start";
-  int cx = M5.Lcd.width() / 2;
-  int screenW = M5.Lcd.width();
-  int screenH = M5.Lcd.height();
-  int centerX = screenW / 2;
-  int logoY = 40; //Change to move
-  int logoX = (screenW / 2) + 50; //Change 50 to move right
-  //Setting portrait landscape
   M5.Lcd.setRotation(3);
-  
+
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextSize(2);
+  M5.Lcd.setTextDatum(MC_DATUM);
+  M5.Lcd.drawString("M5Stick PC Stats",
+                    M5.Lcd.width() / 2,
+                    M5.Lcd.height() / 2 + 20);
 
-  M5.Lcd.setTextDatum(MC_DATUM);   // Middle-center anchor
-  M5.Lcd.drawString(greeting,
-                  M5.Lcd.width() / 2,
-                  M5.Lcd.height() / 2 + 20);
-  M5.Lcd.setTextSize(1);
-  M5.Lcd.drawString(instrucHome,
-                  M5.Lcd.width() / 2,
-                  M5.Lcd.height() / 2 + 40);
+  WiFi.begin(ssid, password);
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.println("Connecting...");
 
-    M5.Lcd.println("Connecting...");
-    WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
 
-    while (WiFi.status() != WL_CONNECTED)
-    {
-      delay(500);
-      Serial.print(".");
-    }
-
-    M5.Lcd.println("WiFi OK");
-    delay(1000);
-
+  M5.Lcd.println("WiFi OK");
 }
 
-void loop() 
+void loop()
 {
   M5.update();
-  
+
   if (M5.BtnA.wasPressed())
   {
-    M5.update();
     M5.Lcd.fillScreen(BLACK);
     stats();
   }
