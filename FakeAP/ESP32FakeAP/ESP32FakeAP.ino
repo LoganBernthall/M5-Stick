fdcb88ec83d76a402a8da2410f45573ed152a613
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2026-03-07 21:49:00 +0000

    Starting Project

---
+++ FakeAP/ESP32FakeAP/ESP32FakeAP.ino
@@ -0,0 +1,19 @@
+﻿<#  
+*Created By - Logan Bernthall - 07/03/2026
+*Fake WiFi AP.
+*TO BE USED FOR EDUCATIONAL PURPOSES ONLY.
+#> 
+
+#include <Wifi.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
