236aec6c693c05cdaba437bd11300c971fc55ab6
LoganBernthall <69209172+LoganBernthall@users.noreply.github.com>
2026-03-16 17:33:01 +0000

    Setup of Intro Screen

---
+++ FakeAP/ESP32FakeAP/ESP32FakeAP.ino/ESP32FakeAP.ino.ino
@@ -0,0 +1,36 @@
+
+#include <M5StickCPlus.h>
+
+void setup() {
+
+  //Start
+  Serial.begin(115200);
+  M5.begin();
+  
+  //Variable declaration
+  char greeting[] = "M5Stick Capitve Portal AP";
+  char instrucHome[] = "Press Home To Start";
+
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextSize(2); 
+
+  M5.Lcd.setTextDatum(MC_DATUM);   // Middle-center anchor
+  M5.Lcd.drawString(greeting,
+                  M5.Lcd.width() / 2,
+                  M5.Lcd.height() / 2 + 20);
+  M5.Lcd.setTextSize(1);
+  M5.Lcd.drawString(instrucHome,
+                  M5.Lcd.width() / 2,
+                  M5.Lcd.height() / 2 + 40);
+
+}
+
+void loop() {
+ 
+ //Start program if start button pressed
+ if (M5.BtnA.wasPressed())
+  {
+  
+  }
+ }
+
